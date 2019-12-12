f70b808ed254e62564bd94721579a7dc357caa68
yeshangqinglu <774284388@qq.com>
2019-12-12 20:16:10 +0800

    第二次提交

--- ssr-camp/server/index.js
+++ ssr-camp/server/index.js
@@ -2,14 +2,24 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
 import express from 'express'
+import {StaticRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import store from '../src/store/store'
 import App from '../src/App'
 
 const app=express()
 app.use(express.static('public'))
-app.get('/',(req,res)=>{
+app.get('*',(req,res)=>{
     // const Page=<App title="开课吧"></App>
     //把react组件，解析成html
-    const content=renderToString(App)
+    const content=renderToString(
+        <Provider store={store}> 
+             <StaticRouter location={req.url}>
+                {App}
+            </StaticRouter>
+        </Provider>
+       
+    )
     //字符串模板
     res.send(`
     <html>
